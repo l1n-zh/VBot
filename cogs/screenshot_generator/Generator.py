911a93ba76235e9dbf4cf265d41c75b5bb12eb03
L1n <89698082+l1n-zh@users.noreply.github.com>
2024-04-26 01:05:27 +0800

    bug fix

--- cogs/screenshot_generator/Generator.py
+++ cogs/screenshot_generator/Generator.py
@@ -5,15 +5,17 @@ import subprocess
 
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-HOST = 'localhost'
+HOST = "localhost"
 PORT = 33334
 server_addr = (HOST, PORT)
-subprocess.run(["node", "./markdown-parser/parser.js"])
+subprocess.Popen(["node", "./cogs/screenshot_generator/markdown-parser/parser.js"])
 
-def parse(message:str) -> str:
+
+def parse(message: str) -> str:
     s.sendto(message.encode(), server_addr)
     result, _ = s.recvfrom(33333)
     return result.decode()
+
 
 class Generator:
     def __init__(self, img_id):
@@ -22,11 +24,11 @@ class Generator:
         self.height = 0
 
     def add(self, name, color, avatar_url, message, time):
-        self.height += (message.count("\n")*100 or 100) + 300
-        if not color.value : color = "white"
+        self.height += (message.count("\n") * 100 or 100) + 300
+        if not color.value:
+            color = "white"
         message = parse(message)
-        self.html += (
-            f"""<div class="discord">
+        self.html += f"""<div class="discord">
                 <img src="{avatar_url}"
                     class="avatar" />
                 <div class="content">
@@ -36,13 +38,16 @@ class Generator:
                             {message}
                         </div>
                 </div>
-            </div>""")
-
+            </div>"""
 
     def generate(self):
         hti = Html2Image(size=(2000, self.height), output_path="./buffer")
         with open("cogs/screenshot_generator/style.css") as stylesheet:
-            hti.screenshot(html_str=self.html, css_str=stylesheet, save_as = f"screenshot{self.img_id}.png")
-    
+            hti.screenshot(
+                html_str=self.html,
+                css_str=stylesheet,
+                save_as=f"screenshot{self.img_id}.png",
+            )
+
     def delete_img(self):
         remove(f"./buffer/screenshot{self.img_id}.png")
